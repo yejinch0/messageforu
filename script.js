8a8920f8084c1914903523abdfe1b65e7552ba03
yejinch0 <129028704+yejinch0@users.noreply.github.com>
2024-06-17 23:58:13 +0900

    script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function selectOption(option) {
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
+        document.getElementById('no-button').innerText = '나 싫오? 여기 그만 놀러'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
